0dc7224646112b1edc27bf5be2dde7cd9387b643
gatordevin <gatordevin@gmail.com>
2020-06-27 15:28:25 -0400

    Created new branch

--- JoystickTest.py
+++
@@ -1,5 +0,0 @@
-from BrooklynAPI.Joystick import Joystick
-
-joystick = Joystick()
-
-print(joystick.get_num_buttons)
--- V2/testuhm.py
+++
@@ -1,6 +0,0 @@
-import serial
-ser = serial.Serial(port="COM4",baudrate=1000000,timeout=None)
-ser.write(bytearray([0,0,0,180,2,45,9,155]))
-while True:
-    ser.write(bytearray([180]))
-    print(ord(ser.read()))
