3cb8fb05cf21bbc827e65834efa454d5fee5d7d4
Devin Willis <gatordevin@gmail.com>
2020-07-10 10:32:05 -0400

    Added handshake byte and ability to send packets
    I can insert extra bytes or even remove them and receive an empty packet or if the error is supported it will get handled by a special error byte.

--- V2/main.py
+++ V2/main.py
@@ -1,8 +1,34 @@
 from serial import Serial
+import atexit
+from time import sleep
 
-brook = Serial('/dev/ttyACM0', 9600) # Establish the connection on a specific port
 
-while True:
+brook = Serial('/dev/ttyACM0', 9600, timeout=0.1) # Establish the connection on a specific port
+brook.write(bytearray([140]))
+brook.read(140)
+
+def exit_handler():
+    while brook.in_waiting:
+        brook.read()
+    brook.write(bytearray([255,1,140,120]))
     resp = brook.read(4)
     resp = [x for x in resp]
     print(resp)
+
+atexit.register(exit_handler)
+
+def send(array):
+    brook.write(bytearray(array))
+    brook.flush()
+    resp = brook.read(4)
+    resp = [x for x in resp]
+    print(resp)
+
+while True:
+    send([255,1,78,120])
+    sleep(1)
+    send([253,1,78,120])
+    sleep(1)    
+
+
+
