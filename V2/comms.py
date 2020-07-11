47b1c7ff23c84ec0e316b42cba18195c40f107ab
Devin Willis <gatordevin@gmail.com>
2020-07-11 15:13:02 -0400

    Updated to now send data back to computer
    Can now add custom data to be sent

--- V2/comms.py
+++ V2/comms.py
@@ -1,7 +1,10 @@
-from serial import Serial
+from serial import Serial, SerialTimeoutException, SerialException
+import serial.tools.list_ports
 import sys
 import struct
 import atexit
+import time
+import termios
 
 class PacketStructure:
     header = 255
@@ -32,24 +35,46 @@ class PacketStructure:
 
 class Comms(Serial):
     def __init__(self, port=None, name=None, baudrate=9600, timeout=0.1, write_timeout=0.1):
-        if port is not None:
-            super().__init__(port=port, baudrate=baudrate, timeout=timeout)
-            if not self.is_open:
+        self.connect_to_device(port, name, baudrate, timeout, write_timeout)
+        if not self.is_open:
                 print("Failed to open serial port")
                 sys.exit()
+        self.handshake(140)
         self.send_packet = PacketStructure()
         self.recv_packet = PacketStructure()
         self.send_packet.packet_id = 0
         atexit.register(self.exit_handler)
 
+    def connect_to_device(self, port, name, baudrate, timeout, write_timeout):
+        if port is not None:
+            super().__init__(port=port, baudrate=baudrate, timeout=timeout, write_timeout=write_timeout)
+            
+    def reconnect_to_device(self):
+        try:
+            ports = list(serial.tools.list_ports.comports())
+            for port in ports:
+                if(port.device==self.port):
+                    self.open()
+                    self.handshake(140)
+                    break
+        except SerialException:
+            print("Failed to connect to device")
+
     def exit_handler(self):
-        while self.in_waiting:
-            self.read()
-        self.send_packet.destination_id = 1
-        self.send_packet.command = 140
-        self.send_packet.data = []
-        self.write_packet()
-        print(self.read_packet())
+        try:
+            while self.in_waiting:
+                self.read()
+            self.send_packet.destination_id = 1
+            self.send_packet.command = 140
+            self.send_packet.data = []
+            self.write_packet()
+            print(self.read_packet())
+        except OSError:
+            self.close()
+            print("Waiting for reconnect")
+            while not self.is_open:
+                self.reconnect_to_device()
+            
    
 
     def handshake(self, handshake_byte):
@@ -63,40 +88,59 @@ class Comms(Serial):
             return False
 
     def write_packet(self, data_type=None):
-        if(self.in_waiting==0 & self.out_waiting==0):
+        
+        data_byte_array = []
+        if data_type is not None:
+            data_byte_array = struct.pack('<'+data_type, *self.send_packet.data)
+        else:
+            data_byte_array = bytearray(self.send_packet.data)
+        self.send_packet.data_length = len(data_byte_array)
+        self.send_packet.generate_checksum(data_byte_array)
+        byte_array = bytes(self.send_packet)
+        try:
             self.reset_input_buffer()
-            data_byte_array = []
-            if data_type is not None:
-                data_byte_array = struct.pack('<'+data_type, *self.send_packet.data)
-            else:
-                data_byte_array = bytearray(self.send_packet.data)
-            self.send_packet.data_length = len(data_byte_array)
-            self.send_packet.generate_checksum(data_byte_array)
-            byte_array = bytes(self.send_packet)
-            print("writing")
             self.write(byte_array)
             self.write(data_byte_array)
-            print("finished writing")
+            send_time = round(time.time())
             self.flush()
             self.send_packet.packet_id += 1
-            return self.send_packet.get_array() + self.send_packet.data
-        else:
-            return []
+            current_time = round(time.time())
+            try:
+                while (self.in_waiting==0):
+                    current_time = round(time.time())
+                    if(self.timeout<(current_time-send_time)):
+                        raise SerialTimeoutException
+            except OSError:
+                print("Input output failed")
+        except SerialTimeoutException:
+            print("write timed out")
+        except termios.error:
+            print("input output error")
+            self.exit_handler()
+        return self.send_packet.get_array() + self.send_packet.data
 
     def read_packet(self, data_type=None):
-        if(self.in_waiting>0 & self.out_waiting==0):
+        try:
             self.reset_output_buffer()
             resp = self.read(len(self.recv_packet))
             if(len(resp)==len(self.recv_packet)):
                 resp = struct.unpack('<BBBLbbL', resp)
                 data_resp = self.read(resp[5])
+                self.flushInput()
                 if(len(data_resp)==resp[5]):
                     if data_type is not None:
+                        print(data_resp)
+                        print(struct.pack('<iI', *[-32345,12345]))
                         data_resp = struct.unpack('<'+data_type, data_resp)
                         return list(resp)+list(data_resp)
                     else:
                         return list(resp)
                 else:
                     return list(resp)
+        except SerialTimeoutException:
+            print("read timed out")
+        except termios.error:
+            print("input output error")
+            self.exit_handler()
         return []
                 
--- V2/test.py
+++ V2/test.py
@@ -1,9 +1,6 @@
 from comms import Comms
-from time import sleep
 
 brook = Comms('/dev/ttyACM0')
-
-brook.handshake(140)
 
 brook.send_packet.destination_id = 1
 brook.send_packet.command = 78
@@ -11,6 +8,5 @@ brook.send_packet.data = [-121.567, 123, -170, -4446744073709551615]
 
 while True:
     write = brook.write_packet(data_type='fIhq')
-    # print("Writing: ", write)
-    recv = brook.read_packet(data_type='iHHHH')
-    # print("Recieving: ", write)
+    recv = brook.read_packet(data_type='f')
+    print(write, recv)
